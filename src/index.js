6f4c7c62a4f56c36696ba07d8866bf91ad908c80
Hasaki1997 <hasaki1997@foxmail.com>
2018-06-14 16:00:32 +0800

    redux-saga-chat

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import reducers from './reducers/index'
+import handleNewMessage from './saga/index'
+import setupSocket from './sockets'
+import username from './utils/name'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+)
+
+const socket = setupSocket(store.dispatch, username)
+
+sagaMiddleware.run(handleNewMessage, { socket, username })
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
